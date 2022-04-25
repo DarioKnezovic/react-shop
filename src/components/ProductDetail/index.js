674acf92d81db00933f7589dec9501dcf19a9c5c
DarioKnezovic <dknezovic997@gmail.com>
2022-04-25 23:53:11 +0200

    Add to cart method introduced.

--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import "./style.css";
 import {Rating} from "@mui/material";
+import CartContext from "../../context/Cart";
 
 const ProductDetail = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const addToCart = () => {
+        cartCtx.updateCartItems(props.product);
+    }
     return (
         <div className="product-detail">
             <div className="row image-row">
@@ -30,7 +36,7 @@ const ProductDetail = (props) => {
                         readOnly />
                     <span className="product-detail__count">({props.product.rating.count})</span>
                 </div>
-                <button className="product-detail__add-to-cart">
+                <button className="product-detail__add-to-cart" onClick={addToCart}>
                     Add to cart
                 </button>
             </div>
--- src/context/Cart.js
+++ src/context/Cart.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 
 const CartContext = React.createContext({
-    items: []
+    items: [],
+    updateCartItems: (item) => {},
 });
 
 export const CartContextProvider = (props) => {
     const [items, updateItems] = useState([]);
 
-    return <CartContext.Provider value={{items}}>{props.children}</CartContext.Provider>
+    const updateCartItems = (item) => {
+        updateItems(old => [...old, item]);
+    }
+
+    return <CartContext.Provider value={{items, updateCartItems}}>{props.children}</CartContext.Provider>
 };
 
 export default CartContext;
